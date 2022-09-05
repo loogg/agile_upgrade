b352f6e7fea4b712f9f391e2464e4390027a6997
malongwei <2544047213@qq.com>
2022-09-05 15:05:16 +0800

    更新 example

--- example/src/test.c
+++ example/src/test.c
@@ -8,8 +8,26 @@
 
 extern const struct agile_upgrade_ops agile_upgrade_file_ops;
 
+static uint32_t _written_len = 0;
+static uint32_t _total_len = 0;
+
+static void step_hook(agile_upgrade_step_t step) { printf("step: %d\r\n", step); }
+
+static void error_hook(agile_upgrade_step_t step, agile_upgrade_err_t code) {
+    printf("step: %d, err: %d\r\n", step, code);
+}
+
+static void progress_hook(uint32_t cur_size, uint32_t total_size) {
+    _written_len = cur_size;
+    _total_len = total_size;
+}
+
 int main(int argc, char *argv[]) {
     if (argc < 2) return -1;
+
+    agile_upgrade_set_step_hook(step_hook);
+    agile_upgrade_set_error_hook(error_hook);
+    agile_upgrade_set_progress_hook(progress_hook);
 
     agile_upgrade_t src_agu = {0};
     src_agu.name = "src";
@@ -22,6 +40,8 @@ int main(int argc, char *argv[]) {
     dst_agu.ops = &agile_upgrade_file_ops;
 
     agile_upgrade_release(&src_agu, &dst_agu, 0);
+
+    printf("Written len: %u, total len: %u\r\n", _written_len, _total_len);
 
     return 0;
 }
