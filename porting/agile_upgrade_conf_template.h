8ea0e68f78800c1ba9694fd8c34fee46bbb4b1fb
malongwei <2544047213@qq.com>
2022-09-20 10:32:12 +0800

    增加 rt-thread 移植文件

--- porting/agile_upgrade_conf_template.h
+++ porting/agile_upgrade_conf_template.h
@@ -1,3 +1,16 @@
+/**
+ * @file    agile_upgrade_conf_template.h
+ * @brief   Agile Upgrade 软件包配置头文件
+ * @author  马龙伟 (2544047213@qq.com)
+ * @date    2022-08-29
+ *
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2022 Ma Longwei.
+ * All rights reserved.</center></h2>
+ *
+ */
+
 #ifndef __AGILE_UPGRADE_CONFIG_H
 #define __AGILE_UPGRADE_CONFIG_H
 
